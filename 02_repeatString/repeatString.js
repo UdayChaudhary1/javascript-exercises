6b964c5a059b1199bc4b656eee155708b0526a2a
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-01-04 15:33:32 +0530

    Complete the exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  answer = "";
+  if(num < 0) return 'ERROR';
+  while(num){
+    answer += string;
+    num--;
+  }
+  return answer;
 };
 
 // Do not edit below this line
